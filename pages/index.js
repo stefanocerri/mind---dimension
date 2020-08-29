8d7063a01ed68b549e82a0c182db4330e7195baa
Stefano Cerri <stefano.cerri@evoluzionetelematica.it>
2020-08-29 18:49:48 +0200

    Add Google font

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return <StyledTitle>____Fuck You</StyledTitle>
 }
+
+
+const StyledTitle = styled.div`
+  margin-top:144px;
+  font-size: ${({ theme }) => theme.fontSizes.hero};
+  color: ${({ theme }) => theme.colors.black};
+`;
---
+++ theme/Theme.js
@@ -0,0 +1,46 @@
+export const theme = {
+    colors : {
+        white: '#fff',
+        black: '#000',
+        gray: '#808080',
+        grayDark: '#0B0B0B',
+        primary: '#FF8A00',
+        secondary: '#59FFA0'
+    },
+
+    fontSizes : {
+        supertiny: '.45rem',
+        tiny: '.65rem',
+        small: '.85rem',
+        p: '1rem',
+        h1: '2.25rem',
+        h2: '2rem',
+        h3: '1.75rem',
+        h4: '1.5rem',
+        h5: '1.25rem',
+        h6: '1rem',
+        hero: '5.5rem',
+        giant: '7rem',
+    },
+
+    flexboxgrid: {
+        // Defaults
+        gridSize: 12, // columns
+        gutterWidth: 0, // rem
+        outerMargin: 0, // rem
+        mediaQuery: 'only screen',
+        container: {
+            sm: "", // rem
+            md: "", // rem
+            lg: ""  // rem
+        },
+        breakpoints: {
+            xs: 0,  // em
+            sm: 48, // em
+            md: 64, // em
+            lg: 75  // em
+        }
+    }
+}
+
+export default theme
